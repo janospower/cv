409e3152598161e4194eef43fdbed71cfa2adcbc
Janos <pauer.janos@gmail.com>
2017-07-11 16:16:33 +0200

    Now successfully changing accent colors according to scrlPos

--- js/index.js
+++ js/index.js
@@ -8,8 +8,18 @@ document.addEventListener('scroll', function(){
     var percent = (docEl[st]||b[st]) / ((docEl[sh]||b[sh]) - window.innerHeight);
 
 	// console.log(percent);
+
   if (0 <= percent && percent <= 1) {
-    
+    var x = document.getElementsByClassName("akzentbg");
+    var i;
+    for (i = 0; i < x.length; i++) {
+      x[i].style.backgroundColor = "hsl("+ (152+ (percent * 360)) +", 89%, 61%)";
+    }
+    var y = document.getElementsByClassName("akzent");
+    var j;
+    for (j = 0; j < y.length; j++) {
+      y[j].style.color = "hsl("+ (152+ (percent * 360)) +", 89%, 61%)";
+    }
   }
 
 
