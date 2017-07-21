871975ed6d331a4cad733ca8d3e4f61425df680c
Janos <pauer.janos@gmail.com>
2017-07-21 18:32:35 +0200

    changed transform origin and behaviour for header

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@ document.addEventListener('scroll', function(){
   if (0 <= percent && percent <= 1) {
 
     var hone = document.getElementsByClassName("headerone");
-      hone[0].style.transform = "rotateX("+(percent*1500)+"deg)";
+      hone[0].style.transform = "rotateX("+(docEl[st]||b[st]*0.6)+"deg)";
 
       // hone[0].style.transform += "translateY("+(percent*2500)+"px)";
 
