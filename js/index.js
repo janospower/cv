346eb25c7ae9d0fcf85cbb6cca6d1ee78ca8674f
janospower <pauer.janos@gmail.com>
2018-02-04 19:02:39 +0100

    grid span instead of lightbox

--- js/index.js
+++ js/index.js
@@ -62,7 +62,7 @@ ready(function(){
 
     e = e || window.event;
     // var lightboxes = document.getElementsByClassName("lightbox");
-    var tags = ["kindl","condom","grenade","spotify","uni","icon","technisch","technisch2","app","sunshine","heart","phone","bvg"]
+    var tags = ["kindl","condom","grenade","spotify","uni","icon","technisch","technisch2","app","sunshine","heart","phone","bvg","julia","mutter"]
     // var as = document.getElementsByClassName("lightbox");
 
     if (e.keyCode == '38' || e.keyCode == '37') {
