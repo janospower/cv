97c48ede93ead2ff93ba68b3efcf27c29b0fd769
janospower <pauer.janos@gmail.com>
2018-02-05 01:06:57 +0100

    try to defer

--- index.php
+++ index.php
@@ -278,5 +278,17 @@
     <div class="back akzentbg">
 
     </div>
+    <script>
+    function init() {
+      var imgDefer = document.getElementsByTagName('img');
+      for (var i = 0; i < imgDefer.length; i++) {
+        if (imgDefer[i].getAttribute('data-src')) {
+          imgDefer[i].setAttribute('src',imgDefer[i].getAttribute('data-src'));
+        }
+      }
+    }
+
+    window.onload = init;
+  </script>
   </body>
 </html>
