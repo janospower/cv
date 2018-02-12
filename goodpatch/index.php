7efa84e6d662ae06a6c67ab0949907d9d4fb731e
janospower <pauer.janos@gmail.com>
2018-02-12 21:24:40 +0100

    initializing goodpatch

---
+++ goodpatch/index.php
@@ -0,0 +1,306 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <!-- metadata -->
+    <title>Janos Pauer &ndash; Goodpatch</title>
+    <meta name="description" content="Janos Pauer Portfolio">
+
+    <meta name = "viewport" content = "width = device-width">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+  	<!-- <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"> -->
+
+    <!-- css -->
+    <link href="http://www.janospauer.com/css/stylesheet.css" rel="stylesheet" type="text/css" />
+    <link href="goodpatch.css" rel="stylesheet" type="text/css" />
+
+    <!-- js -->
+    <script src="http://www.janospauer.com/js/index.js" type="text/javascript"></script>
+
+
+    <!-- fonts -->
+
+    <!-- icons -->
+    <link rel="apple-touch-icon" href="http://www.janospauer.com/images/icon.png" />
+    <link rel="shortcut icon" href="http://www.janospauer.com/favicon.ico" type="http://www.janospauer.com/image/x-icon">
+    <link rel="icon" href="http://www.janospauer.com/favicon.ico" type="image/x-icon">
+    <link rel='mask-icon' href='http://www.janospauer.com/pinned.svg' color='hsl(152, 89%, 61%)'>
+  </head>
+  <body>
+    <main>
+      <article class="white">
+        <header>
+          <h1 class="headerone sentence">Good
+            <div class="slidingVertical">
+              <span class="akzent"> morning!</span>
+              <span class="akzent">patch!</span>
+            </div>
+          </h1>
+        </header>
+
+
+          <!-- <article class="intro"> -->
+            <section class="about">
+              <h2>Who&rsquo;s That?</h2>
+              <p>
+                An aspiring digital product designer based in Berlin.
+                Read a bit more about how I got where I am or jump straight to my <a href="#portfolio" class="akzent">projects</a>.
+              </p>
+            </section>
+
+           <section class="physics">
+             <object data="http://www.janospauer.com/images/physics.svg" class="svgobject inlinesvg" id="phys"></object>
+
+
+             <p>I have a bachelor&rsquo;s degree in physics from the <a href="http://www.fu-berlin.de/en/index.html" class="akzent" target="_blank">Freie Universität Berlin</a>. I studied physics mostly to prove to myself that I could do it. Reasearch for my thesis involved beam time at the <a href="https://www.helmholtz-berlin.de/quellen/bessy/index_en.html" class="akzent" target="_blank"><span class="caps">BESSY II</span></a> synchrotron radiation source.</p>
+             <p class="indent">This is also where I picked up a lot of useful skills like Python, Java and JavaScript, and how to process and visualise data properly.</p>
+           </section>
+
+
+
+           <section class="htw">
+
+             <object data="http://www.janospauer.com/images/design.svg" class="svgobject inlinesvg"></object>
+
+             <p>Right now, I&rsquo;m enrolled in a bachelor&rsquo;s program in <em>communication design</em> at <a href="https://www.htw-berlin.de/en/" class="akzent" target="_blank">HTW Berlin</a>.</p>
+             <p class="indent"> Recently I&rsquo;ve been working on my JavaScript skills in combination with some fancy UI&thinsp;/&thinsp;UX stuff. I love this kind of work because it exposes me to awesome tools and technology like Figma, Framer, <span class="caps">THREE</span>.js and D3.js.
+             </p>
+             <p class="indent">
+               On the side, I am a chairman at <a href"https://www.sehenundernten.org" style="text-transform: lowercase" class="akzent" target="_blank">sehen und ernten</a>.
+             </p>
+           </section>
+
+           <section class="bar">
+
+             <object data="http://www.janospauer.com/images/bar.svg" class="svgobject inlinesvg"></object>
+             <p>I also proudly bartend part-time at <a href="https://www.facebook.com/Loewerein" class="akzent" target="_blank">Alter Roter Löwe Rein</a>. I believe it&rsquo;s important to know how to make a decent gin fizz and how to correctly pour a beer. </br>I really enjoy the comradery of working in a great team, but I will say that the hours are pretty terrible.</p>
+             <p class="indent"></p>
+           </section>
+
+           <section class="rebikel">
+             <object data="http://www.janospauer.com/images/patch.svg" class="svgobject inlinesvg"></object>
+
+
+             <p>Some other usefull skills include repairing bicycles, fluent academic English (<span class="caps">TOEFL</span> score of 112⁄120) and even a driver&rsquo;s license!</p>
+           </section>
+          <!-- </article> -->
+          <!-- <article class="portfolio"> -->
+            <section class="portfolio">
+              <h2>Portfolio</h2>
+              <p>
+                Over the years I have accumulated large amounts of projects&mdash;some better than others. The following is just a glimpse into some of the work that are particularily close to my heart.
+              </p>
+            </section>
+      </article>
+
+
+
+      <article class="entry transparent">
+        <section class="works">
+          <div class="phone">
+            <div class="screen">
+              <video src="http://www.janospauer.com/videos/blcrop.mp4" poster="http://www.janospauer.com/videos/blcrop.png" autoplay loop playsinline="" width="100%" height="100%" class="proto"></video>
+            </div>
+          </div>
+          <div class="text">
+            <h3>Value Driven Graphics</h3>
+              <p><span class="year akzent">2018</span>
+              &ldquo;<a href="https://github.com/janospower/berlinerluft" target="_blank">Berliner Luft</a>&rdquo; is a companion app concept for indoor air quality sensors.<br/>
+              This was a holistic semester project where students were free to choose their own topic and had to undergo each step of product develepment with an emphasis on dynamic vector graphics.
+            </p>
+            <p><strong>Collaborator: </strong>Philine Schell<br/>
+              <strong>Lecturer: </strong>Torsten Malcherczyk<br/>
+              <strong>Type: </strong>Concept | dynamic prototype<br/>
+              <strong>Tools: </strong>Figma, Framer, <span class="caps">THREE</span>.js, D3.js<br/>
+            </p>
+          </div>
+        </section>
+      </article>
+
+      <div class="whiteentry">
+        <article class="entry">
+          <section class="works">
+
+            <div class="text">
+              <h3>Mobility as a Service</h3>
+                <p><span class="year akzentbg">2017</span>
+                  &ldquo;<a href="https://www.figma.com/file/DqUnNAx6WLODxefnQdZZX2Ku/City-Bike-Design-Sprint" target="_blank" class="akzent">cycleto</a>&rdquo; resulted from an intense 5-day design sprint. The theme was mobility and each team of two had to come up with a product, undertake extensive research and visualise their concept under well-defined time constraints.
+              </p>
+              <p><strong>Collaborator: </strong><a href="http://www.louistatschke.com" target="_blank">Louis Tatschke</a><br/>
+                <strong>Lecturer: </strong>Matthias Flucke<br/>
+                <strong>Type: </strong>Concept | Prototype<br/>
+                <strong>Tools: </strong>Figma, Flinto, After Effects<br/>
+              </p>
+            </div>
+            <div class="phone">
+              <div class="screen">
+                <video src="http://www.janospauer.com/videos/cycleto.mp4" poster="http://www.janospauer.com/videos/cycleto.png" autoplay loop playsinline="" width="100%" height="100%" class="proto"></video>
+              </div>
+            </div>
+          </section>
+        </article>
+      </div>
+
+      <article class="entry transparent">
+        <section class="works">
+          <ul class="tabs" onclick="switcher(0)">
+            <li class="startseite"><strong>Landing Page</strong></li>
+            <li class="designdetails"><strong>Design Details</strong></li>
+            <hr class="" />
+          </ul>
+          <div class="browsers" onclick="switcher(0)">
+            <div class="browser top">
+              <div class="screen">
+                <video src="http://www.janospauer.com/videos/start.mp4" poster="http://www.janospauer.com/videos/start.png" autoplay loop playsinline="" width="100%" height="100%" class="proto"></video>
+              </div>
+            </div>
+            <div class="browser bottom b">
+              <div class="screen">
+                <video src="http://www.janospauer.com/videos/dd.mp4" poster="http://www.janospauer.com/videos/dd.png" autoplay loop playsinline="" width="100%" height="100%" class="proto"></video>
+              </div>
+            </div>
+          </div>
+          <h3>Technology I</h3>
+          <div class="text">
+              <p><span class="year akzent">2017</span>
+              &ldquo;Techno&rdquo; was a full-class-effort to build a solid foundation in web technologies by collaborating on an online magazine.<br/>
+              Me and my team built the <a href="https://github.com/hanspagel/htw-magazin-gruppe-1/tree/master/Startseite" target="_blank">landing page</a>, as well as <a href="https://github.com/hanspagel/htw-magazin-gruppe-1/tree/master/Design%20Details" target="_blank">an article</a> on a podcast called &ldquo;Design Details&rdquo;.
+            </p>
+          </div>
+          <div class="text">
+            <p><strong>Collaborators: </strong>Yannic Scheffel & Miriam Zils<br/>
+              <strong>Lecturer: </strong>Hans Pagel<br/>
+              <strong>Type: </strong>Two Static websites<br/>
+              <strong>Tools: </strong>Sketch, fullPage.js, GitHub, lots of vanilla JavaScript<br/>
+            </p>
+          </div>
+        </section>
+      </article>
+
+      <div class="whiteentry">
+        <article class="entry transparent">
+          <section class="works">
+            <div class="ipad browsers">
+              <div class="screen">
+                <video src="http://www.janospauer.com/videos/todo.mp4" poster="http://www.janospauer.com/videos/todo.png" autoplay loop playsinline="" width="100%" height="100%" class="proto"></video>
+              </div>
+            </div>
+            <h3>Digital Media</h3>
+            <div class="text">
+                <p><span class="year akzentbg">2017</span>
+                &ldquo;<span class="caps">TODO</span>&rdquo; is the result of my first ever course on digital media.<br/>
+                After little exercises like designing icons, wireframes and information architectures, this was our vision for a to-do iPad app, tailored for K–12 schools.
+              </p>
+            </div>
+            <div class="text">
+              <p><strong>Collaborators: </strong>Joao Wichert & Rebecca Merdes<br/>
+                <strong>Lecturer: </strong>Andreas Ingerl<br/>
+                <strong>Type: </strong>UI | prototype<br/>
+                <strong>Tools: </strong>Sketch, Flinto<br/>
+              </p>
+            </div>
+          </section>
+        </article>
+      </div>
+
+      <article class="entry transparent">
+        <section class="works">
+          <div class="image">
+            <iframe src="http://www.janospauer.com/svg/index.html"></iframe>
+          </div>
+          <h3>Programmatic Glyph Generation</h3>
+          <div class="text">
+              <p><span class="year akzent">2017</span>
+               An &ldquo;alphabet&rdquo; of hundreds of millions of possible characters, created to order by the browser for a course on typography.
+            </p>
+          </div>
+          <div class="text">
+            <p><strong>Lecturer: </strong>Prof. Jürgen Huber<br/>
+              <strong>Type: </strong>Generative experimental typography<br/>
+              <strong>Tools: </strong>SVG, JavaScript<br/>
+            </p>
+          </div>
+        </section>
+      </article>
+
+      <div>
+        <div class="entry transparent">
+          <h3>Assorted Projects</h3>
+        </div>
+        <article class="entry transparent rest">
+
+
+          <section class="tile">
+              <object data="http://www.janospauer.com/images/spotify2.svg" class="svgobject worksimg"></object>
+          </section>
+
+
+
+          <section class="tile">
+              <object data="http://www.janospauer.com/images/senf.svg" class="svgobject worksimg"></object
+            </a>
+          </section>
+
+
+
+          <?php
+          $path = "http://www.janospauer.com/images/works";
+          $glob1 = glob($path . '/*');
+          // print_r($glob1);
+          //
+          // $glob2 = new GlobIterator($path . '/*');
+          // print_r(iterator_to_array($glob2));
+
+          for ($i=0; $i<count($glob1); $i++)
+          {
+            // print_r($i);
+            // print_r($num);
+            $num = $glob1[$i];
+            echo '<section class="tile">
+            <img src="data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" data-src="'.$num.'" class="worksimg" alt="Gallery image">
+            </section>'." ";
+          }
+          ?>
+
+        </article>
+      </div>
+
+
+    </main>
+
+    <footer>
+      <div class="fcont">
+        <ul>
+          <li><a href="https://www.instagram.com/janosp/" target="_blank">
+          <img src="data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" data-src="http://www.janospauer.com/images/instagram.svg" class="socialicon" alt="Instagram Icon"></a></li>
+          <li><a href="https://open.spotify.com/user/1171628059" target="_blank">
+            <img src="data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" data-src="http://www.janospauer.com/images/spotify.svg" class="socialicon" alt="Spotify Icon"></a></li>
+          <li><a href="mailto:&#109;&#101;&#064;&#106;&#097;&#110;&#111;&#115;&#112;&#097;&#117;&#101;&#114;&#046;&#099;&#111;&#109;?Subject=janospauer.com">
+            <img src="data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" data-src="http://www.janospauer.com/images/mail.svg" class="socialicon" alt="Mail Icon"></a></li>
+          <li><a href="www.linkedin.com/in/janospauer">
+            <img src="data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" data-src="http://www.janospauer.com/images/linkedin.svg" class="socialicon" alt="LinkedIn Icon"></a></li>
+        </ul>
+
+        <div class="copy">
+          <a href="http://www.janospauer.com/imprint/imprint.html" alt="Imprint">Imprint</a> | &copy; Janos Pauer
+        </div>
+
+      </div>
+    </footer>
+    <div class="back akzentbg">
+
+    </div>
+    <script>
+    function init() {
+      var imgDefer = document.getElementsByTagName('img');
+      for (var i = 0; i < imgDefer.length; i++) {
+        if (imgDefer[i].getAttribute('data-src')) {
+          imgDefer[i].setAttribute('src',imgDefer[i].getAttribute('data-src'));
+        }
+      }
+    }
+
+    window.onload = init;
+  </script>
+  </body>
+</html>
